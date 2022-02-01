a66058c2ac599546d23ecfc33b08d619c6c6e181
beatrisebertule <97166445+beatrisebertule@users.noreply.github.com>
2022-02-01 16:38:58 +0200

    Add project files.

---
+++ TestNo3/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestNo3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hi, my name is Korg. I'm kind of like the leader here.");
+        }
+    }
+}
